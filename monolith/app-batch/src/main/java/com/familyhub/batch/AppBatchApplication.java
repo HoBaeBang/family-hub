cd4d369e09a2690653cc0b2b4fbd216fff0ef1d3
HobaeBang <bhb0407@naver.com>
2026-04-12 21:07:06 +0900

    phase 1 : project setting

---
+++ monolith/app-batch/src/main/java/com/familyhub/batch/AppBatchApplication.java
@@ -0,0 +1,12 @@
+package com.familyhub.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBatchApplication.class, args);
+    }
+}
---
+++ monolith/app-batch/src/test/java/com/familyhub/batch/AppBatchApplicationTest.java
@@ -0,0 +1,12 @@
+package com.familyhub.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBatchApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
